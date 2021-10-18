db77010a48f3fa3f54f2a653030be73fdd3515f8
kamand shaigan <k.shaigan@narvanstartupstudio.com>
2021-10-19 01:20:18 +0330

    config

--- bookmark_codebase/lib/main.dart
+++ bookmark_codebase/lib/main.dart
@@ -1,11 +1,12 @@
 
 import 'package:bookmark_codebase/utils/main/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
